3c076a0eadad4643c03c59f6f566cb263aab491b
Renan Moura <rvmoura.96@gmail.com>
2018-02-07 11:29:16 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este repositório contem os exercícios e arquivos desenvolvidos durante o curso introdutorio a JS pela alura
---
+++ js/main.js
@@ -0,0 +1,3 @@
+var title = document.querySelector(".title");
+title.textContent = "Aparecida Nutricionista";
+console.log(title);
