18fb96563f8faa6a9ee6c4f6ebb4eb33bdc9246b
Renan Moura <rvmoura.96@gmail.com>
2018-02-07 20:25:05 -0200

    Exercícios concluídos dos módulos 2 e 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 var title = document.querySelector(".title");
 title.textContent = "Aparecida Nutricionista";
-console.log(title);
+
+var testPatient = document.querySelectorAll(".paciente");
+console.log(testPatient);
+for(var i = 0 ; i < testPatient.length ; i++){
+    
+    var patient = testPatient[i];
+
+    var tdWeight = patient.querySelector(".info-peso");
+    var weight = tdWeight.textContent;
+
+    var tdHeight = patient.querySelector(".info-altura");
+    var height = tdHeight.textContent;
+
+    var tdImc = patient.querySelector(".info-imc"); 
+
+    var isWeight = true;
+    var isHeight = true;
+
+    if(weight <= 0 || weight >= 1000){
+        console.log("peso inválido");
+        isWeight = false;
+        tdImc.textContent = "Peso Inválido";
+        patient.classList.add("invalid-patient")
+    }
+
+    if(height <= 0 || height >= 3.0){
+        console.log("altura inválida");
+        isHeight = false;
+        tdImc.textContent = "Altura Inválido";
+        patient.classList.add("invalid-patient")
+    }
+
+    if(isWeight && isHeight){
+        var imc = weight/Math.pow(height,2);
+        
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
